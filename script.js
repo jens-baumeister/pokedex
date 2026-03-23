926b8cef103d464403159c8129bd5071ffc82716
Jens Baumeister <jens.baumeister@gmx.net>
2026-03-23 11:22:37 +0100

    add function load

---
+++ script.js
@@ -0,0 +1,9 @@
+// function init(){
+//     document.getElementsByClassName("loader").style.display = "block"
+//     document.getElementsByClassName("card").style.display = "none"
+// }
+
+function load() {
+    document.getElementById('card').style.display = "none"
+    document.getElementById('loader').style.display = ""
+}
