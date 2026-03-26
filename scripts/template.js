3762ed5750329bd88b4ee6aa453093538deb9cc3
Jens Baumeister <jens.baumeister@gmx.net>
2026-03-26 11:43:59 +0100

    clean template with variables

--- scripts/template.js
+++ scripts/template.js
@@ -5,13 +5,15 @@ async function getPokeCard(i) {
   let id = data.id;
   let name = data.name;
   let typesHTML = await getTypeIcons(data.types);
+
+  endOfLoading();
   
   return `
 <section class="card">
-    <div class="card-header">#${id} ${pokeJson[i].name.charAt(0).toUpperCase(1) + pokeJson[i].name.slice(1).toLowerCase()}</div>
+    <div class="card-header">#${id} ${name.charAt(0).toUpperCase(1) + name.slice(1).toLowerCase()}</div>
     <div class="card-body">
         <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-viii/brilliant-diamond-shining-pearl/${id}.png"
-                alt="picture of ${pokeJson[i].name}">
+                alt="picture of ${name}">
     </div>
     <div class="card-footer">${typesHTML}</div>
 </section>`;
