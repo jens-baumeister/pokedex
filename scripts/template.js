6bfe382c411c6d6c20b63bacc97151d024b3aa5e
Jens Baumeister <jens.baumeister@gmx.net>
2026-03-26 12:19:24 +0100

    add type relative background-color

--- scripts/template.js
+++ scripts/template.js
@@ -5,13 +5,14 @@ async function getPokeCard(i) {
   let id = data.id;
   let name = data.name;
   let typesHTML = await getTypeIcons(data.types);
+  let mainType = data.types[0].type.name;
 
   endOfLoading();
   
   return `
 <section class="card">
     <div class="card-header">#${id} ${name.charAt(0).toUpperCase(1) + name.slice(1).toLowerCase()}</div>
-    <div class="card-body">
+    <div class="card-body ${mainType}">
         <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-viii/brilliant-diamond-shining-pearl/${id}.png"
                 alt="picture of ${name}">
     </div>
